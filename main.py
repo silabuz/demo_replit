88e22a7c648722078f21906536f14e26f78ffa28
jsparedes <>
2022-09-15 12:52:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a = True
+a = str(1)
+b = "2"
+c = a + b
+print(a+b)
+print(type(c))
---
+++ main1.py
@@ -0,0 +1,10 @@
+a = "Hola mundo"
+# procesos de cálculo
+a = True
+print(a)
+print("El valor de a es:", a)
+print("El tipo de dato de a es:", type(a))
+
+# Comparación
+# Python: logical = True
+# Rust: let logical: bool = true;
